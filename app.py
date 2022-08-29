d9e2bc7bf7f899a30ed38c546ed6211ce279b7e3
ryanjerome22 <ryanjeromevalencia22@gmail.com>
2022-08-29 11:11:56 +0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+channel = "PinoyFreecoder"
+channel_num= 35
+is_active= False
+average = 3.5
+
+print (channel)
+
+name = input ("Enter a name: ")
+age = input ("Input a number: ")
+print("Hello " + name + " Your age is " + age)
