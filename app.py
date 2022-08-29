9a369ebccc97886d7483096469206efba467bae7
ryanjerome22 <ryanjeromevalencia22@gmail.com>
2022-08-29 11:32:07 +0800

    Second Commit

--- app.py
+++ app.py
@@ -5,6 +5,29 @@ average = 3.5
 
 print (channel)
 
-name = input ("Enter a name: ")
-age = input ("Input a number: ")
-print("Hello " + name + " Your age is " + age)
+
+def addnumber(num1,num2):   #defining a function
+    total= num1 + num2
+    return (total)
+
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+print("Total: ", addnumber(num1, num2))
+
+
+class Student:
+    def check_pass_fail (self):
+        if self.marks >= 40:
+            return True
+        else:
+            return False
+
+    def __init__(self, name, marks):
+            self.name = name
+            self.marks = marks
+
+student1 = Student ("Harry", 85)
+student2 = Student ("Janet", 30)
+did_pass = student1.check_pass_fail()
+
+print(did_pass)
