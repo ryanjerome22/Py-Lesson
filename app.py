75e57b260530119fc1a40f45ceff3ad0dbdce5e4
ryanjerome22 <ryanjeromevalencia22@gmail.com>
2022-08-31 19:53:12 +0800

    08-31,2022 Lesson

--- app.py
+++ app.py
@@ -5,29 +5,6 @@ average = 3.5
 
 print (channel)
 
-
-def addnumber(num1,num2):   #defining a function
-    total= num1 + num2
-    return (total)
-
-num1 = int(input("Please enter your first number: "))
-num2 = int(input("Please enter your second number: "))
-print("Total: ", addnumber(num1, num2))
-
-
-class Student:
-    def check_pass_fail (self):
-        if self.marks >= 40:
-            return True
-        else:
-            return False
-
-    def __init__(self, name, marks):
-            self.name = name
-            self.marks = marks
-
-student1 = Student ("Harry", 85)
-student2 = Student ("Janet", 30)
-did_pass = student1.check_pass_fail()
-
-print(did_pass)
+my_num=-5.5
+new_num= abs(my_num)
+print(int(new_num))
