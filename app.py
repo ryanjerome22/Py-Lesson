07e1fed2b4ecdc45bde39627f525ee85814d06d8
ryanjerome22 <ryanjeromevalencia22@gmail.com>
2022-09-06 06:15:06 +0800

    Try commit

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+
+# importing Libraries
+from math import  * 
+
 channel = "PinoyFreecoder"
 channel_num= 35
 is_active= False
@@ -8,3 +12,6 @@ print (channel)
 my_num=-5.5
 new_num= abs(my_num)
 print(int(new_num))
+
+print(floor(new_num))
+
