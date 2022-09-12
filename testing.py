2eec5cb258a4d38735c22bd62acbb77b6798c773
ryanjerome22 <ryanjeromevalencia22@gmail.com>
2022-09-12 14:16:33 +0800

    New update

--- testing.py
+++ testing.py
@@ -13,4 +13,26 @@ Hotdog1.Introduce ()
 
 
 
+class FB:
+    def __init__(self, FirstName, LastName, FbLikes, FbFriends):
+        self.FirstName= FirstName
+        self.LastName= LastName
+        self.FbLikes= FbLikes
+        self.Fbfriends= FbFriends
+
+    def NewAccount (self):
+            print ("FB Account: ")
+            print(" My Name is : " + self.FirstName + self.LastName)
+            print(" FB Likes: " + str(self.FbLikes))
+            print (" FB Friends: ")
+
+            for friend in self.Fbfriends:
+                print ("   --" + friend)
+
+FB1 = FB ("Ryan Jerome ", "Valencia ", 23, ["Christine Nisa", "Norely M. Valencia"])
+
+FB1.NewAccount()
+
+
+
 
