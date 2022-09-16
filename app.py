a852da9755ed0261c78b7ed9b9a862e4ed38d9be
ryanjerome22 <ryanjeromevalencia22@gmail.com>
2022-09-16 18:03:52 +0800

    new update

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# Sample Class
+from turtle import done
+
+
 class Warrior:
     def __init__(self, name, health, mana):
         self.name= name
@@ -8,9 +12,27 @@ class Warrior:
 Warrior1= Warrior("RJ", 100, 50)
 print (Warrior)
 
-
+# is instance function
 name = int("20")
 
 print(isinstance(name,int))
 
+#Ternary Operator
 
+def is_adult (age):
+    if age > 18:
+        return True
+    else:
+        return False
+
+def is_adult2 (age):
+    return True if age > 18 else False
+
+
+done= True
+
+def example ():
+    if done:
+        return ("yes")
+    else:
+        return ("no")
